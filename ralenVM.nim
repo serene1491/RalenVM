27d73a9a5592f8236ecf9592e0e90d23bebde827
serene1491 <isabelasuper2009@gmail.com>
2025-09-27 09:27:41 -0400

    reverse check order for files

--- ralenVM.nim
+++ ralenVM.nim
@@ -6,11 +6,12 @@ when isMainModule:
     if paramCount() < 1: repl()
     let path = paramStr(1)
 
+    if not (path.len >= 6 and path[^6 .. ^1] == ".ralen"):
+        echo "Invalid extension. Expected an .ralen file"
+        quit(1)
+    
     if not fileExists(path):
         echo "The file doesn't exists: " & path
-        quit(1)
-    if not path.len >= 6 and path[^6 .. ^1] == ".ralen":
-        echo "Invalid extension. Expected an .ralen file"
         quit(1)
 
     let src = readFile(path)    
