a32f310b1957ab4fcadf80291c2f4bc2480729b2
serene1491 <isabelasuper2009@gmail.com>
2025-09-27 09:17:33 -0400

    allow to open files

--- ralenVM.nim
+++ ralenVM.nim
@@ -1,3 +1,17 @@
+import std/os
 import src/repl
+import src/frontend/parser
 
-repl()
+when isMainModule:
+    if paramCount() < 1: repl()
+    let path = paramStr(1)
+
+    if not fileExists(path):
+        echo "The file doesn't exists: " & path
+        quit(1)
+    if path.len >= 6 and path[^6 .. ^1] == ".ralen":
+        echo "Invalid extension. Expected an .ralen file"
+        quit(1)
+
+    let src = readFile(path)    
+    parse(src)
